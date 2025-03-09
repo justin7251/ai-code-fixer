8242d531939a35a44adf83ff2a7f8f412ff4e0e1
justin725 <myjustinleung19@hotmail.com>
2025-03-09 08:37:26 +0000

    update

--- functions/auth/index.js
+++ functions/auth/index.js
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
     res.setHeader('X-Content-Type-Options', 'nosniff');
     res.setHeader('X-Frame-Options', 'DENY');
     res.setHeader('X-XSS-Protection', '1; mode=block');
+    console.log(`[DEBUG] ${req.method} ${req.path} from origin: ${req.headers.origin}`);
     next();
 });
 
